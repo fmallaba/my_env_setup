7355b5fc60c11c6409fcadc96e8a44338437406b
Furkat Mallabaiev <bomcrimea@gmail.com>
2020-07-03 19:04:35 +0300

    Add bg and more automatation
    - Add bg setup
    - Add bash completion case insensitive
    - Add git setup
    - Add readme

---
+++ README.md
@@ -0,0 +1,1 @@
+curl -LJO https://raw.githubusercontent.com/fmallaba/my_env_setup/master/setup.sh
--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
 
 function setup_bash {
     echo "[ -f $PWD/bash/.my_bash_addons ] && source $PWD/bash/.my_bash_addons" >> ~/.bashrc
+    sudo echo "set completion-ignore-case on" >> /etc/inputrc
 }
 
 function setup_vim {
@@ -22,11 +24,31 @@ function setup_terminator {
     ln -s $PWD/terminator ~/.config/terminator
 }
 
+function setup_bg {
+    gsettings set org.gnome.desktop.background picture-uri file://$PWD/bg/jungle_bg.jpg
+}
+
+function install_chrome {
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    sudo apt install ./google-chrome-stable_current_amd64.deb -y
+}
+
+function setup_git {
+    sudo apt install git -y
+    sudo apt install meld -y
+    git config --global user.email "bomcrimea@gmail.com"
+    git config --global user.name "Furkat Mallabaiev"
+    git config --global merge.tool meld
+}
+
 function setup_all {
+    setup_git
     setup_bash
     setup_vim
     setup_mc
     setup_terminator
+    setup_bg
+    install_chrome
 }
 
 sudo -
