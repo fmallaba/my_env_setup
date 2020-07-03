29f33b4f8c9d4686d3067d7362c8285234dd0e65
Furkat Mallabaiev <bomcrimea@gmail.com>
2020-07-03 19:17:58 +0300

    Add setup download

--- setup.sh
+++ setup.sh
@@ -1,38 +1,5 @@
 #!/bin/bash
 
-function setup_bash {
-    echo "[ -f $PWD/bash/.my_bash_addons ] && source $PWD/bash/.my_bash_addons" >> ~/.bashrc
-    sudo echo "set completion-ignore-case on" >> /etc/inputrc
-}
-
-function setup_vim {
-    sudo apt install vim-gnome -y
-    rm -rf ~/.vim ~/.vimrc
-    git clone https://github.com/VundleVim/Vundle.vim.git vim/.vim/bundle/Vundle.vim
-    ln -s $PWD/vim/.vim ~/.vim
-    ln -s $PWD/vim/.vimrc ~/.vimrc
-    vim +PluginInstall +qall > /dev/null
-}
-
-function setup_mc {
-    sudo apt install mc -y
-    ln -s $PWD/mc ~/.config/mc
-}
-
-function setup_terminator {
-    sudo apt install terminator -y
-    ln -s $PWD/terminator ~/.config/terminator
-}
-
-function setup_bg {
-    gsettings set org.gnome.desktop.background picture-uri file://$PWD/bg/jungle_bg.jpg
-}
-
-function install_chrome {
-    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-    sudo apt install ./google-chrome-stable_current_amd64.deb -y
-}
-
 function setup_git {
     sudo apt install git -y
     sudo apt install meld -y
@@ -41,15 +8,18 @@ function setup_git {
     git config --global merge.tool meld
 }
 
-function setup_all {
-    setup_git
-    setup_bash
-    setup_vim
-    setup_mc
-    setup_terminator
-    setup_bg
-    install_chrome
+SETUP_DIR=~/Desktop/.my_env_setup
+
+function download_my_env_setup {
+    git clone https://github.com/fmallaba/my_env_setup.git $SETUP_DIR
+}
+
+function run_setup_env {
+    cd $SETUP_DIR
+    ./setup_env.sh
 }
 
 sudo -
-setup_all
+setup_git
+download_my_env_setup
+run_setup_env
---
+++ setup_env.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+function setup_bash {
+    echo "[ -f $PWD/bash/.my_bash_addons ] && source $PWD/bash/.my_bash_addons" >> ~/.bashrc
+    sudo echo "set completion-ignore-case on" >> /etc/inputrc
+}
+
+function setup_vim {
+    sudo apt install vim-gnome -y
+    rm -rf ~/.vim ~/.vimrc
+    git clone https://github.com/VundleVim/Vundle.vim.git vim/.vim/bundle/Vundle.vim
+    ln -s $PWD/vim/.vim ~/.vim
+    ln -s $PWD/vim/.vimrc ~/.vimrc
+    vim +PluginInstall +qall > /dev/null
+}
+
+function setup_mc {
+    sudo apt install mc -y
+    ln -s $PWD/mc ~/.config/mc
+}
+
+function setup_terminator {
+    sudo apt install terminator -y
+    ln -s $PWD/terminator ~/.config/terminator
+}
+
+function setup_bg {
+    gsettings set org.gnome.desktop.background picture-uri file://$PWD/bg/jungle_bg.jpg
+}
+
+function install_chrome {
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    sudo apt install ./google-chrome-stable_current_amd64.deb -y
+}
+
+function setup_all {
+    setup_bash
+    setup_vim
+    setup_mc
+    setup_terminator
+    setup_bg
+    install_chrome
+}
+
+sudo -
+setup_all
